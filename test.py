eade27102a3a5e4a11cd3f6f10f3b28e73430827
OmarSaidAH <148548122+OmarSaidAH@users.noreply.github.com>
2025-09-09 19:38:48 +0200

    Update .gitignore to include test_db_connect.py and remove test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from databricks.connect import DatabricksSession
-
-spark = DatabricksSession.builder.serverless().profile("dev").getOrCreate()
-
-df = spark.read.table("samples.nyctaxi.trips")
-df.show(5)
